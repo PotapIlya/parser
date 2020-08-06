0e286b8bb9c112bcc3086e2939f6837cfc36031c
potap <PotapIlya>
2020-08-06 14:05:46 +0300

    da

--- index.php
+++ index.php
@@ -1,18 +1,16 @@
 <?php
 
-$curl = curl_init();
-$url = 'https://freelancehunt.com/projects?name=laravel&tags%5B%5D=php&tags%5B%5D=javascript&tags%5B%5D=html&tags%5B%5D=CSS%2FHTML&tags%5B%5D=Vue.js';
+$curl = curl_init('http://tester.loc/cookie.php');
 
-
-
-curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($curl, CURLOPT_DNS_CACHE_TIMEOUT, 30);
+curl_setopt($curl, CURLOPT_HEADER, true);
+
+curl_setopt($curl, CURLOPT_COOKIEJAR, '/file.txt');
+
 
 $html = curl_exec($curl);
 
-$url = '|data-published=.*?|sei';
+curl_close($curl);
 
-preg_match_all($url, $html, $arr);
 
-print_r($arr);
+echo $html;
