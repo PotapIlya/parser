9fc3523e60593e7e0a733d1078035e1b9d204620
potap <PotapIlya>
2020-08-06 13:14:13 +0300

    12

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 $curl = curl_init();
 $url = 'https://freelancehunt.com/projects?name=laravel&tags%5B%5D=php&tags%5B%5D=javascript&tags%5B%5D=html&tags%5B%5D=CSS%2FHTML&tags%5B%5D=Vue.js';
 
+
+
 curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_DNS_CACHE_TIMEOUT, 30);
